25ccb2d93e0f1c872f7726093140fdac0ec9477f
Dorumuk <haulinef@gmail.com>
2025-01-15 19:28:38 +0900

    feat: update blog paths and hero images to include 'blog-for-you' prefix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/blog-for-you/blog/${post.id}/`,
 		})),
 	});
 }
